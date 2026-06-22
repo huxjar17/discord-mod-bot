76992e2602ec34c7776f74d73d705b3229dcd6c3
Hux J <huxjar17@gmail.com>
2026-06-22 15:12:45 -0700

    force vercel to trigger new deployment

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -1,3 +1,13 @@
+// Force Vercel Redeploy 1
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist'
+  }
+})
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
